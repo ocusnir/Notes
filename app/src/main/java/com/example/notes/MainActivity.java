95018f9665d11fe2873416a53810e3168955a1e8
ocusnir <102429445+ocusnir@users.noreply.github.com>
2022-12-16 16:36:11 +0200

    linked with firebase made authentication

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -4,25 +4,96 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.button.MaterialButton;
+
+import java.util.regex.Pattern;
+
+import javax.xml.validation.Validator;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailEditText, passwordEditText, confirmPasswordEditText;
+    MaterialButton createAccountButton;
+    ProgressBar progressBar;
+    TextView loginButtonTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = findViewById(R.id.register);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (button.isPressed()){
-                    Toast.makeText(MainActivity.this, "Your FastNote account has been created!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+        createAccountButton = findViewById(R.id.register_button);
+        progressBar = findViewById(R.id.progress_bar);
+        loginButtonTextView = findViewById(R.id.login_text_button);
+
+        createAccountButton.setOnClickListener(v-> createAccount());
+        loginButtonTextView.setOnClickListener(v-> finish());
+
+//        final MaterialButton materialButton = findViewById(R.id.register_button);
+//        materialButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (materialButton.isPressed()){
+//                    Toast.makeText(MainActivity.this, "Your FastNote account has been created!", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
     }
-}
+    protected void createAccount(){
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+
+        boolean isValidated = validateData(email, password, confirmPassword);
+        if(isValidated){
+            return;
+        }
+        createAccountInFirabase(email,password);
+
+    }
+
+    protected void createAccountInFirabase(String email, String password){
+
+
+    }
+    void changeInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            createAccountButton.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            createAccountButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    boolean validateData(String email, String password, String confirmPassword){
+        //validate data that is input by user.
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Email is invalid");
+            return false;
+        }
+        if(password.length()<5){
+            passwordEditText.setError("Password is too short");
+            return false;
+        }
+        if(!password.equals(confirmPassword)){
+            confirmPasswordEditText.setError("Password not matched");
+            return false;
+        }
+        return true;
+
+        }
+    }
