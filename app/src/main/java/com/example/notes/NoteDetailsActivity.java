703671215179bae91da2cb752791d0011e383f2e
ocusnir <102429445+ocusnir@users.noreply.github.com>
2022-12-21 20:02:53 +0200

    modified NoteDetailsActivity class.

--- app/src/main/java/com/example/notes/NoteDetailsActivity.java
+++ app/src/main/java/com/example/notes/NoteDetailsActivity.java
@@ -1,14 +1,35 @@
 package com.example.notes;
 
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class NoteDetailsActivity extends AppCompatActivity {
+
+    EditText NoteTitle, NoteDescription;
+    ImageButton SaveNoteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_details);
+
+        NoteTitle = findViewById(R.id.note_title);
+        NoteDescription = findViewById(R.id.note_description);
+        SaveNoteBtn = findViewById(R.id.save_note_btn);
+
+        SaveNoteBtn.setOnClickListener((v)-> saveNote());
+
+    }
+
+    void saveNote(){
+        String noteTitle = NoteTitle.getText().toString();
+        String noteDesc = NoteDescription.getText().toString();
+
+        if(noteTitle==null || noteTitle.isEmpty()){
+            NoteTitle.setText("");
+        }
     }
 }
